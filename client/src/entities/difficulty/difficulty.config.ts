ff20ee20b3cf989e7b3418848b3bc97d77553ba9
Daniil Petrov <daniilpetrov@Daniils-MacBook-Air.local>
2026-04-27 21:32:27 +0300

    small addition

--- client/src/entities/difficulty/difficulty.config.ts
+++ client/src/entities/difficulty/difficulty.config.ts
@@ -1,4 +1,4 @@
-import type { Difficulty } from "./difficulty";
+import type { Difficulty, DifficultyName } from "./difficulty";
 
 export const EASY_DIFFICULTY: Difficulty = {
     name: "EASY",
@@ -9,3 +9,8 @@ export const HARD_DIFFICULTY: Difficulty = {
     name: "HARD",
     drinksPerRound: [2, 4, 6, 8]
 }
+
+export const DIFFICULTY_MAP: Record<DifficultyName, Difficulty> = {
+  EASY: EASY_DIFFICULTY,
+  HARD: HARD_DIFFICULTY,
+};
