e3ad16460b7822b3e701b52acc94d5111b8b1bc6
Daniil Petrov <daniil.petrov030@gmail.com>
2026-05-13 21:24:04 +0300

    WIP

---
+++ client/src/components/Players/PlayerRow.tsx
@@ -0,0 +1,34 @@
+import {
+  IconArrowBadgeLeftFilled,
+  IconArrowBadgeRightFilled,
+  IconUserCircle,
+} from "@tabler/icons-react";
+import "./players.css";
+
+export function PlayerRow() {
+  return (
+    <div className="player-row pixel-corners">
+      <button className="player-row__arrow-button" type="button">
+        <IconArrowBadgeLeftFilled size={50} color="#f6c453" />
+      </button>
+
+      <div className="player-row__avatar">
+        <IconUserCircle stroke={2} size={60} />
+      </div>
+
+      <div className="player-row__content">
+        <span className="player-row__index pixel-corners">P1</span>
+
+        <input
+          type="text"
+          placeholder="PLAYER 1 NAME"
+          className="player-row__input"
+        />
+      </div>
+
+      <button className="player-row__arrow-button" type="button">
+        <IconArrowBadgeRightFilled size={50} color="#f6c453" />
+      </button>
+    </div>
+  );
+}
--- client/src/components/Players/PlayersTab.tsx
+++ client/src/components/Players/PlayersTab.tsx
@@ -1,24 +1,26 @@
 import { Title } from "../random/Title";
+import { PlayerRow } from "./PlayerRow";
 import "./players.css";
 
 export function PlayersTab() {
-    return (
-        <section className="players-tab">
-            <div className="players-tab__title">
-            <Title/>
-            </div>
+  return (
+    <section className="players-tab">
+      <div className="players-tab__title">
+        <Title />
+      </div>
 
-            <div className="players-tab__rows">
+      <div className="players-tab__rows">
+        <PlayerRow />
+        <PlayerRow />
+        <PlayerRow />
+        <PlayerRow />
+      </div>
 
-            </div>
-
-            <div className="players-tab__buttons">
-                <button className="test-pixel-border">
-  Press start 1p
-</button>
-                <button>3</button>
-                <button>4</button>
-            </div>
-        </section>
-    );
+      <div className="players-tab__buttons">
+        <button className="test-pixel-border">Press start 1p</button>
+        <button>3</button>
+        <button>4</button>
+      </div>
+    </section>
+  );
 }
