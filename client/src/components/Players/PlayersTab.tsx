cd99f764b4fea93ea8f9c578910724b52891e2f4
Daniil Petrov <daniil.petrov030@gmail.com>
2026-05-16 23:39:43 +0300

    feat: added player select component

--- client/src/components/Players/PlayersTab.tsx
+++ client/src/components/Players/PlayersTab.tsx
@@ -16,10 +16,45 @@ export function PlayersTab() {
         <PlayerRow />
       </div>
 
+      <span className="players-select-text">SELECT PLAYERS: </span>
+
       <div className="players-tab__buttons">
-        <button className="test-pixel-border">Press start 1p</button>
-        <button>3</button>
-        <button>4</button>
+        <button
+          className="secondary-pixel-corners players-tab__player-count-button"
+          type="button"
+        >
+          2
+        </button>
+        <button
+          className="secondary-pixel-corners players-tab__player-count-button"
+          type="button"
+        >
+          3
+        </button>
+        <button
+          className="secondary-pixel-corners players-tab__player-count-button"
+          type="button"
+        >
+          4
+        </button>
+        <button
+          className="secondary-pixel-corners players-tab__player-count-button"
+          type="button"
+        >
+          5
+        </button>
+        <button
+          className="secondary-pixel-corners players-tab__player-count-button"
+          type="button"
+        >
+          6
+        </button>
+        <button
+          className="secondary-pixel-corners players-tab__player-count-button"
+          type="button"
+        >
+          7
+        </button>
       </div>
     </section>
   );
