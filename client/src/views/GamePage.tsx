f1f7b1bcb7c37e7ab831e906c34dd196ff4e92b4
Daniil Petrov <daniil.petrov030@gmail.com>
2026-06-03 19:44:23 +0300

    Fixed bug, where user would not be redirected back when the gameState was empty

--- client/src/views/GamePage.tsx
+++ client/src/views/GamePage.tsx
@@ -1,15 +1,11 @@
-// TODO: In next ticket will get the gameState from the localStorage
-
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { getGameState } from "../entities/gameStorage";
 
 export function GameView() {
-  const navigate = useNavigate();
-
   const gameState = getGameState();
 
   if (!gameState) {
-    navigate("/");
+    return <Navigate to="/" replace />;
   }
 
   return <section className="difficulty-tab">GAME TIMEEEE</section>;
